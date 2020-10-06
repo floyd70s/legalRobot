82dee78e3719949b5d02792b6110e9f1ed9acb9f
floyd70s <claudioperezdiaz@gmail.com>
2020-10-06 03:56:30 -0300

    connect to Sqlite

--- visualstudioselenium/Program.cs
+++ visualstudioselenium/Program.cs
@@ -6,66 +6,140 @@ using iText.Pdfocr.Tesseract4;
 using System.Net;
 using System.IO;
 using Newtonsoft.Json;
+using System.Configuration;
+using System.Collections.Generic;
 using System.Data;
+using System.Data.SQLite;
+using Microsoft.Data.Sqlite;
+
+using System.Text;
 
 namespace suseso
 {
     class MainClass
     {
-        private static readonly Tesseract4OcrEngineProperties tesseract4OcrEngineProperties = new Tesseract4OcrEngineProperties();
 
         public static void Main(string[] args)
         {
 
 
+            string conStringSQLite = ConfigurationManager.ConnectionStrings["conStringSQLite"].ConnectionString;
+            string PJRobotSQlite = ConfigurationManager.AppSettings["PJRobotSQlite"];
             string iniDate = "2019-09-29";//DateTime.Now.ToString("yyyy/MM/dd"); 
             string endDate = "2020-09-29";// DateTime.Now.AddDays(-365).ToString("yyyy/MM/dd");
+            int start = 0;
+            int group = 0;
+            string jResult = "-";
 
-            string URL = "https://suseso-engine.newtenberg.com/mod/find/cgi/find.cgi?action=jsonquery&" +
-                         "engine=SwisheFind&rpp=16&" +
-                         "cid=512&" +
-                         "iid=612&" +
-                         "pnid_search=&searchon=aid&properties=546,523,525,532,620&json=1&keywords=&" +
-                         "pnid546_desde=" + iniDate +
-                         "&pnid546_hasta=" + endDate +
-                         "&start=0&group=1&show_ancestors=1&searchmode=and&pvid_and=500%3A510&aditional_query=%27%20cid%3D(512)%27%20-L%" +
-                         "20property-value.546.iso8601%20" + iniDate +
-                         "%20" + endDate +
-                         "T23%3A59%3A59%20-s%20property-value.546.iso8601%20desc%20title%20desc&" +
-                         "callback=jQuery20009428819093757522_1601343927812&_=1601343927813";
+            while (jResult != "")
+            {
+                string URL = "https://suseso-engine.newtenberg.com/mod/find/cgi/find.cgi?action=jsonquery&" +
+                             "engine=SwisheFind&rpp=16&" +
+                             "cid=512&" +
+                             "iid=612&" +
+                             "pnid_search=&searchon=aid&properties=546,523,525,532,620&json=1&keywords=&" +
+                             "pnid546_desde=" + iniDate +
+                             "&pnid546_hasta=" + endDate +
+                             "&start=0" + start +
+                             "&group=" + group +
+                             "&show_ancestors=1&searchmode=and&pvid_and=500%3A510&aditional_query=%27%20cid%3D(512)%27%20-L%" +
+                             "20property-value.546.iso8601%20" + iniDate +
+                             "%20" + endDate +
+                             "T23%3A59%3A59%20-s%20property-value.546.iso8601%20desc%20title%20desc&" +
+                             "callback=jQuery20009428819093757522_1601343927812&_=1601343927813";
 
-            string siteBase = extractWebSuseso(URL);
+                string siteBase = extractWebSuseso(URL);
+                int iniJson = siteBase.IndexOf("[");
+                int endJson = siteBase.IndexOf("]");
 
-            extractinfoSuseso(siteBase);
-            //string text = ExtractTextFromPDF("/Users/claudioperez/Documents/programacion/legalRobot/visualstudioselenium/PDFLoaded/R22-2019.pdf");
+                jResult = siteBase.Substring(iniJson, endJson - iniJson + 1);
+                DataTable dtResult = extractInfoSuseso(jResult);
+                DataTable dtJurAdministrativa = GetData(PJRobotSQlite, "select * from planilla");
+                group++;
+            }
+
             Console.WriteLine("-----------------------------------------------------------------");
 
         }
 
-        public static void extractinfoSuseso(string siteBase)
+        /// <summary>
+        /// this function transform a JSON and return datatable
+        /// </summary>
+        /// <param name="jResult">JSON with results from website</param>
+        /// <returns>return datatable with results from website</returns>
+        public static DataTable extractInfoSuseso(string jResult)
         {
-            int iniJson = siteBase.IndexOf("[");
-            int endJson = siteBase.IndexOf("]");
-
-            string jResult = siteBase.Substring(iniJson, endJson - iniJson + 1);
-            dynamic json = JsonConvert.DeserializeObject(jResult);
-
             DataTable dt = (DataTable)JsonConvert.DeserializeObject(jResult, typeof(DataTable));
+            return dt;
+        }
 
-            for (int i = 0; i <= dt.Rows.Count - 1; i++)
+
+        /// <summary>
+        /// generic function for execute SQL command with SQLite BDDriver.
+        /// </summary>
+        /// <param name="query">query to execute</param>
+        /// <param name="connectionString"></param>
+        /// <returns>return DataTable with result</returns>
+        public static DataTable GetData(string connectionString, string query)
+        {
+            DataTable dt = new DataTable();
+            Microsoft.Data.Sqlite.SqliteConnection connection;
+            Microsoft.Data.Sqlite.SqliteCommand command;
+
+            connection = new Microsoft.Data.Sqlite.SqliteConnection("Data Source= /Users/claudioperez/Documents/programacion/legalRobot/visualstudioselenium/BD/PJRobots.sqlite");
+            try
             {
-                string sAid = dt.Rows[i]["aid"].ToString();
-                string sFilePDF = "https://www.suseso.cl/612/articles-" + sAid + "_archivo_01.pdf";
-
-                using (WebClient webClient = new WebClient())
-                {
-                    webClient.DownloadFile(sFilePDF, "/Users/claudioperez/Downloads/PDF/"+ sAid + "_archivo_01.pdf");
-                    Console.WriteLine("Successfully Downloaded File \"{0}\" from \"{1}\"", sAid, sFilePDF);
-                }
+                connection.Open();
+                command = new Microsoft.Data.Sqlite.SqliteCommand(query, connection);
+                dt.Load(command.ExecuteReader());
+                connection.Close();
+            }
+            catch
+            {
             }
 
-            Console.WriteLine("----end");
+            return dt;
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /// <summary>
+        /// save PDF from website SUSESO with AID
+        /// </summary>
+        /// <param name="PDFPath">path for save PDF file</param>
+        /// <param name="sAid">unique ID for PDF file</param>
+        public static void savePdf(string PDFPath, string sAid)
+        {
+
+            string sFilePDF = "https://www.suseso.cl/612/articles-" + sAid + "_archivo_01.pdf";
+
+            using (WebClient webClient = new WebClient())
+            {
+                webClient.DownloadFile(sFilePDF, PDFPath + sAid + "_archivo_01.pdf");
+                Console.WriteLine("Successfully Downloaded File \"{0}\" from \"{1}\"", sAid, sFilePDF);
+            }
+        }
+
+
 
 
         public static string extractWebSuseso(string URL)
